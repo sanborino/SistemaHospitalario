751ef971eebd60aeb4afa4e82568e4dda0266351
Ricardo Mondragon <linuxubuntu@hotmail.com>
2026-03-19 16:59:20 -0600

    se corrige password_reset

--- SistemaHospitalario/urls.py
+++ SistemaHospitalario/urls.py
@@ -9,6 +9,7 @@ from django.http import HttpResponseNotFound
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('', include("acceso.urls")),
+    path('acceso/', include('django.contrib.auth.urls')),
     path('hospital/', include("hospital.urls")),
     path('pacientes/', include("paciente.urls", namespace="paciente")),
     path("historial/", include("historial.urls")),
