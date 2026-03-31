5d3f649bfe3392735b9195156620e383f5df82b7
Ricardo Mondragon <linuxubuntu@hotmail.com>
2026-03-31 14:50:49 -0600

    corrige trigger

--- hospitalizacion/migrations/0003_trigger_postgresql.py
+++ hospitalizacion/migrations/0003_trigger_postgresql.py
@@ -6,7 +6,10 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("hospital", "0002_remove_cama_habitacion_alter_asignacioncama_cama_and_more"),
+        (
+            "hospitalizacion",
+            "0002_remove_cama_habitacion_alter_asignacioncama_cama_and_more",
+        ),
     ]
 
     operations = [
