cc18f242412659975f19a0ac7159ef0a5e8276a2
Ricardo Mondragon <linuxubuntu@hotmail.com>
2026-04-01 15:51:12 -0600

    correccion en asignar camaa

--- hospitalizacion/urls.py
+++ hospitalizacion/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "hospitalizacion"
 
 urlpatterns = [
-    path("asignar/<int:paciente_id>/", views.asignar_cama, name="asignar_cama"),
+    path("asignar_cama/<int:paciente_id>/", views.asignar_cama, name="asignar_cama"),
     path("liberar/<int:asignacion_id>/", views.liberar_cama, name="liberar_cama"),
     path("trasladar/<int:asignacion_id>/", views.trasladar_cama, name="trasladar_cama"),
     path("historial/", views.historial_asignaciones, name="historial_asignaciones"),
